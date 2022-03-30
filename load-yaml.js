93b03898a0f62b9b8374ac1e60d5b7d5928501f2
Yuichi Sawada <4645011+yuichi10@users.noreply.github.com>
2022-03-30 20:56:38 +0900

    Update load-yaml.js

--- load-yaml.js
+++ load-yaml.js
@@ -2,7 +2,7 @@
 const { execSync } = require('child_process');
 const yaml = require('js-yaml');
 
-const resultYaml = execSync(./test.yaml);
+const resultYaml = execSync('./test.yaml');
 
 const result = yaml.load(resultYaml);
 console.log(result);
