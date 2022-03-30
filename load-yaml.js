65ab007b9aff54f3ee58ccc97b8c2cee362da867
Yuichi Sawada <4645011+yuichi10@users.noreply.github.com>
2022-03-30 20:54:52 +0900

    Create load-yaml.js

---
+++ load-yaml.js
@@ -0,0 +1,8 @@
+'use strict';
+const { execSync } = require('child_process');
+const yaml = require('js-yaml');
+
+const resultYaml = execSync(./test.yaml);
+
+const result = yaml.load(resultYaml);
+console.log(result);
